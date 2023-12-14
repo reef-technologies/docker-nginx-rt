c70d1cc9afdd973d076692624d12f00ce4f161c0
Michał Nowacki <77497496+mpnowacki-reef@users.noreply.github.com>
2023-12-14 11:22:57 +0100

    Update 50-letsencrypt-renew.sh

--- entrypoint/50-letsencrypt-renew.sh
+++ entrypoint/50-letsencrypt-renew.sh
@@ -10,7 +10,7 @@ cron_dir=""
 
 certbot_file="${cron_dir}/certbot"
 echo "Installing Let's Encrypt crontab script into $certbot_file"
-echo "#!/bin/sh" >> "$certbot_file"
+echo "#!/bin/sh" > "$certbot_file"
 echo "certbot -q --nginx renew" >> "$certbot_file"
 chmod +x "$certbot_file"
 
